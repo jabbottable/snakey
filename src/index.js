a90b13b6e1a2a413970ae99846d4ab391c406d65
Josh Abbott <josh.abbott@live.com>
2019-06-16 21:34:41 +0100

    Circle snake

--- src/index.js
+++ src/index.js
@@ -1,95 +1,104 @@
-const upKey = 38;
-const downKey = 40;
-const leftKey = 37;
-const rightKey = 39;
-const spaceKey = 32;
+const UP_KEY = 38;
+const DOWN_KEY = 40;
+const LEFT_KEY = 37;
+const RIGHT_KEY = 39;
+const SPACE_KEY = 32;
 
 class Game {
-    constructor(boardSize) {
-        this.resetGame(boardSize)
+    constructor(boardSize, canvas, snakeBodySize) {
+        this.canvas = canvas;
         this.highScore = 0;
+        this.boardSize = boardSize;
+        this.snakeBodySize = snakeBodySize;
+        this.resetGame();
     }
-
-    resetGame(boardSize) {        
+    resetGame() {
         this.snake = [];
-        this.snakePositions = makeBoard(boardSize);
-    
-        var middlePoint = boardSize / 2;
-    
+        this.snakePositions = this.makeBoard(this.boardSize);
+        var middlePoint = this.boardSize / 2;
         for (let snakePosition = 3; snakePosition >= 0; snakePosition--) {
-            var snakeBody = new Pixel(canvas, middlePoint - snakePosition, middlePoint, "red");
+            var snakeBody = new Pixel(this.canvas, middlePoint - snakePosition, middlePoint, this.snakeBodySize);
             this.snakePositions[middlePoint - snakePosition][middlePoint] = 1;
             this.snake.push(snakeBody);
         }
-    
         this.getNewRandomFoodPosition(this.snakePositions);
         this.isGameOver = false;
         this.frameTime = 150;
         this.score = 0;
-        this.currentKey = rightKey;
+        this.currentKey = RIGHT_KEY;
         this.lastKeyDrawn = this.currentKey;
     }
-
     getNewRandomFoodPosition() {
         var size = this.snakePositions.length;
         var newFoodPosition = [];
-    
         do {
             newFoodPosition[0] = Math.floor(Math.random() * Math.floor(size));
             newFoodPosition[1] = Math.floor(Math.random() * Math.floor(size));
         } while (this.snakePositions[newFoodPosition[0]][newFoodPosition[1]] == 1);
-    
         this.foodPosition = newFoodPosition;
     }
-
     snakeHead() {
         return this.snake[this.snake.length - 1];
     }
-
     getNextPositions() {
         var nextXPos = this.snakeHead().posX;
         var nextYPos = this.snakeHead().posY;
         switch (this.currentKey) {
-            case upKey:
+            case UP_KEY:
                 nextYPos--;
                 break;
-            case downKey:
+            case DOWN_KEY:
                 nextYPos++;
                 break;
-            case leftKey:
+            case LEFT_KEY:
                 nextXPos--;
                 break;
-            case rightKey:
+            case RIGHT_KEY:
                 nextXPos++;
                 break;
         }
-        return { nextXPos, nextYPos };
+        return {
+            nextXPos,
+            nextYPos
+        };
     }
-
     willSnakeEatSelf(nextXPos, nextYPos) {
         return this.snakePositions[nextXPos][nextYPos] == 1;
+    }
+
+    makeBoard(boardSize) {
+        return Array.from({
+                length: this.boardSize
+            }, () =>
+            Array.from({
+                length: this.boardSize
+            }, () => 0)
+        );
+    }
+}
+
+class Pixel {
+    constructor(canvas, posX, posY, snakeBodySize) {
+        this.posX = posX;
+        this.posY = posY;
+        this.context = canvas.getContext("2d");
+        this.snakeBodySize = snakeBodySize;
+        this.radius = snakeBodySize / 2;
+    }
+    draw(colour) {
+        // this.context.beginPath();
+        this.context.beginPath();
+        this.context.arc((this.posX * this.snakeBodySize) + this.radius, (this.posY * this.snakeBodySize) + this.radius, this.radius - 1, 0, 2 * Math.PI);
+        this.context.strokeStyle = colour;
+        this.context.stroke();
+        // this.context.fillRect(this.posX * this.snakeBodySize, this.posY * this.snakeBodySize, this.snakeBodySize - 1, this.snakeBodySize - 1);
+        this.context.fillStyle = colour;
+        this.context.fill();
     }
 }
 
 const snakeBodySize = 20;
 const boardSize = 20;
-
-class Pixel {
-    constructor(canvas, posX, posY, colour) {
-        this.posX = posX;
-        this.posY = posY;
-        this.colour = colour;
-        this.context = canvas.getContext("2d");
-    }
-
-    draw() {
-        this.context.beginPath();
-        this.context.fillStyle = this.colour;
-
-        this.context.fillRect(this.posX * snakeBodySize, this.posY * snakeBodySize, snakeBodySize - 1, snakeBodySize - 1);
-        this.context.stroke();
-    }
-}
 
 var canvas = document.getElementById("myCanvas");
 
@@ -98,12 +107,10 @@ canvas.width = boardSize * snakeBodySize;
 
 var currentFrameTime = 0;
 
-const validMoveKeys = [upKey, downKey, leftKey, rightKey];
-
-var game = new Game(boardSize);
+var game = new Game(boardSize, canvas, snakeBodySize);
 
 game.isGameOver = true;
-drawText("Welcome to Snakey!!\n\nPress space to\nbegin the adventure");
+drawText("Welcome to Snakey!\n\nPress space to\nbegin the adventure");
 
 function update(progress) {
     if (game.isGameOver) {
@@ -111,7 +118,7 @@ function update(progress) {
     }
     currentFrameTime += progress;
 
-    if (currentFrameTime > game.frameTime) { 
+    if (currentFrameTime > game.frameTime) {
         if (game.foodPosition[0] == game.snakeHead().posX && game.foodPosition[1] == game.snakeHead().posY) {
             game.getNewRandomFoodPosition(game.snakePositions);
             game.frameTime -= 1;
@@ -122,18 +129,21 @@ function update(progress) {
             game.snakePositions[snakeEnd.posX][snakeEnd.posY] = 0;
         }
 
-        var { nextXPos, nextYPos } = game.getNextPositions();
+        var {
+            nextXPos,
+            nextYPos
+        } = game.getNextPositions();
         game.lastKeyDrawn = game.currentKey;
 
         if (isSnakeOfScreen(nextXPos, nextYPos) || game.willSnakeEatSelf(nextXPos, nextYPos)) {
-            if(game.score > game.highScore) {
+            if (game.score > game.highScore) {
                 game.highScore = game.score;
                 updateHighScore(game.highScore)
             }
             game.isGameOver = true;
             drawText("Game Over!\nPress space");
         } else {
-            game.snake.push(new Pixel(canvas, nextXPos, nextYPos, "red"))
+            game.snake.push(new Pixel(canvas, nextXPos, nextYPos, snakeBodySize))
             game.snakePositions[nextXPos][nextYPos] = 1;
             currentFrameTime = 0;
         }
@@ -146,12 +156,12 @@ function isSnakeOfScreen(nextXPos, nextYPos) {
 
 function draw() {
     canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
-    var food = new Pixel(canvas, game.foodPosition[0], game.foodPosition[1], "blue");
-    game.snake.forEach(snakeBody => {
-        snakeBody.draw();
+    game.snake.forEach(function(snakeBody, index) {
+        snakeBody.draw(index % 2 ? "darkgreen" : "green");
     });
 
-    food.draw();
+    var food = new Pixel(canvas, game.foodPosition[0], game.foodPosition[1], snakeBodySize);
+    food.draw("brown");
 }
 
 function loop(timestamp) {
@@ -172,31 +182,23 @@ window.requestAnimationFrame(loop)
 document.addEventListener("keydown", logKey);
 
 function logKey(e) {
+    const VALID_MOVE_KEYS = [UP_KEY, DOWN_KEY, LEFT_KEY, RIGHT_KEY];
+
     var requestedKey = e.keyCode;
 
-    if (validMoveKeys.includes(requestedKey)) {
-        if ((game.lastKeyDrawn == upKey && requestedKey != downKey) ||
-            (game.lastKeyDrawn == downKey && requestedKey != upKey) ||
-            (game.lastKeyDrawn == leftKey && requestedKey != rightKey) ||
-            (game.lastKeyDrawn == rightKey && requestedKey != leftKey)) {
+    if (VALID_MOVE_KEYS.includes(requestedKey)) {
+        if ((game.lastKeyDrawn == UP_KEY && requestedKey != DOWN_KEY) ||
+            (game.lastKeyDrawn == DOWN_KEY && requestedKey != UP_KEY) ||
+            (game.lastKeyDrawn == LEFT_KEY && requestedKey != RIGHT_KEY) ||
+            (game.lastKeyDrawn == RIGHT_KEY && requestedKey != LEFT_KEY)) {
             game.currentKey = requestedKey;
-            return; 
+            return;
         }
     }
 
-    if (game.isGameOver && requestedKey == spaceKey) {
-        game.resetGame(boardSize);
+    if (game.isGameOver && requestedKey == SPACE_KEY) {
+        game.resetGame();
     }
-}
-
-function makeBoard(boardSize) {
-    return Array.from({
-            length: boardSize
-        }, () =>
-        Array.from({
-            length: boardSize
-        }, () => 0)
-    );
 }
 
 function drawText(text) {
@@ -204,7 +206,7 @@ function drawText(text) {
     var ctx = canvas.getContext("2d");
     ctx.fillStyle = "black";
 
-    const lineHeight = canvas.height/15;
+    const lineHeight = canvas.height / 15;
     const fontHeight = Math.floor(lineHeight * 0.9);
     ctx.font = fontHeight + "px Arial";
     ctx.textAlign = "center";
